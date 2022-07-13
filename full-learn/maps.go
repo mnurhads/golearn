7c6339144c3f00a10f626d7fd722906caf861027
mnurhads <nurhadi@nominavipindonesia.com>
2022-07-13 21:00:35 +0700

    push fuc maps & slices

--- full-learn/maps.go
+++ full-learn/maps.go
@@ -4,6 +4,74 @@ import (
 	"fmt"
 )
 
+// func new
+func printEmployee(employees map[int]string, employeeId int) {
+	if name, ok := employees[employeeId]; ok {
+		fmt.Printf("name = %s, ok = %v\n", name, ok)
+	} else {
+		fmt.Printf("EmployeeId %d not found\n", employeeId)
+	}
+}
+
+func isEmployeeExists(employees map[int]string, employeeId int) bool {
+	_, ok := employees[employeeId]
+	return ok
+}
+
+// end
 func main() {
-	fmt.Println("Belum Saya Tambahkan :)")
+	var m = make(map[string]int)
+
+	fmt.Println(m)
+
+	if m == nil {
+		fmt.Println("m is nil")
+	} else {
+		fmt.Println("m is not nil")
+	}
+
+	m["one hundred"] = 100
+	fmt.Println(m)
+
+	// maps literal
+	fmt.Println("----------------------------------")
+	var angka = map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5, 
+	}
+
+	fmt.Println(angka)
+
+	fmt.Println("----------------------------")
+	// maps key
+	var tinderMatch = make(map[string]string)
+
+	// Adding keys to a map
+	tinderMatch["budi"] = "Angelina"
+	tinderMatch["James"] = "Sophia"
+	tinderMatch["David"] = "Emma"
+
+	fmt.Println(tinderMatch)
+
+	// match
+	tinderMatch["budi"] = "Jennifer"
+	fmt.Println(tinderMatch)
+
+	// map exist
+	fmt.Println("----------------------------------")
+	var employees = map[int]string{
+		1001: "Muhammad",
+		1002: "Nur",
+		1003: "Hadi",
+	}
+
+	printEmployee(employees, 1001)
+	printEmployee(employees, 1010)
+
+	if isEmployeeExists(employees, 1002) {
+		fmt.Println("EmployeeId 1002 found")
+	}
 }
--- full-learn/slices.go
+++ full-learn/slices.go
@@ -5,5 +5,45 @@ import (
 )
 
 func main() {
-	fmt.Println("Belum saya tambahkan juga :)")
+	// varian
+	var a = [5]string{"Alpha", "Beta", "Gamma", "Delta", "Epsilon"}
+
+	// Creating a slice from the array
+	var s []string = a[1:4]
+
+	fmt.Println("Array a = ", a)
+	fmt.Println("Slice s = ", s)
+
+	slice1 := a[1:4]
+	slice2 := a[:3]
+	slice3 := a[2:]
+	slice4 := a[:]
+
+	fmt.Println("slice1 = ", slice1)
+	fmt.Println("slice2 = ", slice2)
+	fmt.Println("slice3 = ", slice3)
+	fmt.Println("slice4 = ", slice4)
+
+	//slice other
+	fmt.Println("----------------------------------------")
+	cities := []string{"Jermani", "Sundong", "Prindavan", "Beijing", "Jogja", "Malang", "Bangalore", "Hyderabad", "Hong Kong"}
+
+	asianCities := cities[3:]
+	indoCities := asianCities[1:3]
+
+	fmt.Println("cities = ", cities)
+	fmt.Println("asianCities = ", asianCities)
+	fmt.Println("indoCities = ", indoCities)
+
+	// SLice Program
+	fmt.Println("---------------------------------------")
+	bahasa1 := []string{"C", "C++", "Java"}
+	bahasa2 := append(bahasa1, "Python", "Ruby", "Go")
+
+	fmt.Printf("bahasa1 = %v, len = %d, cap = %d\n", bahasa1, len(bahasa1), cap(bahasa1))
+	fmt.Printf("bahasa2 = %v, len = %d, cap = %d\n", bahasa2, len(bahasa2), cap(bahasa2))
+
+	slice1[0] = "C#"
+	fmt.Println("\nbahasa1 = ", bahasa1)
+	fmt.Println("bahasa2 = ", bahasa2)
 }
